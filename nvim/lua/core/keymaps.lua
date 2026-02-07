c29de42f65666aa69ecef209e407a9418eb21bc8
haku7a <v.a.neyaskin@gmail.com>
2026-02-08 00:34:21 +0300

    feat: add diagnostic quickfix keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -2,3 +2,6 @@ local map = vim.keymap.set
 
 -- Clear search highlights with <Esc>
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+map("n", "<leader>q", vim.diagnostic.setloclist, { silent = true, desc = "Open diagnostic [Q]uickfix list" })
