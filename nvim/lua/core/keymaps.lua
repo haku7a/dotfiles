4920698646fa4c60ca9659572e9d751ed008d696
haku7a <v.a.neyaskin@gmail.com>
2026-02-08 01:40:39 +0300

    feat: add keymap to exit terminal mode

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -5,3 +5,6 @@ map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
 map("n", "<leader>q", vim.diagnostic.setloclist, { silent = true, desc = "Open diagnostic [Q]uickfix list" })
+
+-- Exit terminal mode
+map("t", "<Esc><Esc>", "<C-\\><C-n>", { silent = true, desc = "Exit terminal mode" })
