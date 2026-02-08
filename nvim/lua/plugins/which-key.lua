9019d929e44de24c591d400aab67e2bee91f23e8
haku7a <v.a.neyaskin@gmail.com>
2026-02-09 01:22:55 +0300

    ui: move which-key popup to right side

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -5,7 +5,16 @@ return {
 	opts = {
 		delay = 0,
 		icons = { mappings = vim.g.have_nerd_font },
-
+		win = {
+			width = 45,
+			height = { min = 5, max = 25 },
+			col = -2,
+			row = 0.9,
+			-- border = "rounded",
+			wo = {
+				winblend = 40,
+			},
+		},
 		spec = {
 			{ "<leader>s", group = "[S]earch", mode = { "n", "v" } },
 			{ "<leader>t", group = "[T]oggle" },
