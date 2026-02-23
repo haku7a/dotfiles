f02d06d0495b8dfe67934167736001072395f73f
haku7a <v.a.neyaskin@gmail.com>
2026-02-24 02:20:04 +0300

    feat: add conform.nvim for Python formatting

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      python = { 
+      "ruff_fix",
+      "ruff_organize_imports",
+      "ruff_format" 
+      }
+     },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback"
+    }
+  }
+}
