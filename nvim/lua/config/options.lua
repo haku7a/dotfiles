b1325aa4dfcaf0b47e89e1b35945284af40e732e
haku7a <v.a.neyaskin@gmail.com>
2026-02-23 13:58:05 +0300

    ui: improve line display with numbers and hide eob tildes

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,8 @@ local opt = vim.opt
 
 -- Hide default command line
 opt.cmdheight = 0
+
+-- Line number settings
+opt.number = true          
+opt.relativenumber = true
+opt.fillchars = { eob = " " }
