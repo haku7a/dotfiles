4f8d17b50eaae2ad27b7994151f894637139c76b
haku7a <v.a.neyaskin@gmail.com>
2026-02-23 10:04:38 +0300

    ui: add bufferline.nvim for tabline

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  opts = {
+    options = {
+      show_buffer_close_icons = false,
+    }
+  }
+}
+ 
