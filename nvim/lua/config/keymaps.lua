f3fc8db1cb72ebaafa7d0123b7dbe993585babbf
haku7a <v.a.neyaskin@gmail.com>
2026-02-23 10:18:40 +0300

    feat: add buffer switching Tab/S-Tab and close <Leader>x

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,7 @@
 local map = vim.keymap.set
 
 map("n", "<Leader>w", ":w<CR>", { desc = "Save file" })
+map("n", "<Tab>", ":bnext<CR>", { silent = true, desc = "Next buffer" })
+map("n", "<S-Tab>", ":bprevious<CR>", { silent = true, desc = "Previous buffer" })
+map("n", "<Leader>x", ":bdelete<CR>", { silent = true, desc = "Close current buffer" })
 
