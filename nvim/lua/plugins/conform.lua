08863ffe4e7bdbc1904ca278f2a5a747ef4c828d
haku7a <v.a.neyaskin@gmail.com>
2026-02-24 04:43:35 +0300

    feat: add StyLua formatter for Lua

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,15 +2,23 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
-      python = { 
-      "ruff_fix",
-      "ruff_organize_imports",
-      "ruff_format" 
-      }
-     },
+      python = {
+        "ruff_fix",
+        "ruff_organize_imports",
+        "ruff_format",
+      },
+      lua = {
+        "stylua",
+      },
+    },
+    formatters = {
+      stylua = {
+        prepend_args = { "--indent-width", "2", "--indent-type", "Spaces" },
+      },
+    },
     format_on_save = {
       timeout_ms = 500,
-      lsp_format = "fallback"
-    }
-  }
+      lsp_format = "fallback",
+    },
+  },
 }
