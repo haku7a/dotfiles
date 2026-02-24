09f89925ef40b98c99f9ffb1e1675c727449c215
haku7a <v.a.neyaskin@gmail.com>
2026-02-24 23:46:02 +0300

    feat: add nvim-cmp

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,20 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    local cmp = require("cmp")
+
+    cmp.setup({
+      sources = {
+        { name = "nvim_lsp" },
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+    })
+  end,
+}
